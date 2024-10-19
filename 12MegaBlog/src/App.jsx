f24fb6ac7f66f6faa966bbbbf4167074f9241c81
Faiz Ahmad <faizahmad43430@gmail.com>
2024-10-19 12:52:48 +0530

    01megablog

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+import conf from './conf/conf'
+
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl)
+  console.log(conf.appwriteBucketId)
+  
+
+  return (
+    <>
+    <h1>Hello Jupiter</h1>
+
+    </>
+  )
+}
+
+export default App

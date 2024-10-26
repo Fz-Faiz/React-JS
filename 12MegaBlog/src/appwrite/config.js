2d00684eabeca146eafeed2017d0bb4c1be09e00
Faiz Ahmad <faizahmad43430@gmail.com>
2024-10-27 02:54:31 +0530

    03megaBlog

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,143 @@
+import conf from "../conf/conf";
+import { Client, Databases,Query,Storage, ID } from "appwrite";
+
+export class Services{
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId );
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+        
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try{
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+    
+        }catch(error){
+            console.log(error)
+    
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try{
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true
+
+        }
+        catch(error){
+            console.log(error)
+            return false
+        }
+    }
+    async getPost(slug){
+        try{
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteDatabaseId,
+                queries,
+
+            )
+
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+
+        }
+        catch(error){
+            console.log(error)
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+
+
+}
+
+
+
+const services = new Services()
+
+export default services

23d016d51373b88508ac34648b968b8f8b228748
Faiz Ahmad <faizahmad43430@gmail.com>
2024-10-27 20:06:49 +0530

    04megablog

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,18 +1,44 @@
 import conf from './conf/conf'
 
 import './App.css'
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import{Header,Footer} from './components/index.js'
+import { Outlet } from 'react-router-dom'
 function App() {
-  console.log(conf.appwriteUrl)
-  console.log(conf.appwriteBucketId)
-  
 
-  return (
-    <>
-    <h1>Hello Jupiter</h1>
+  const[loading,setLoading] = useState(true)
+  const dispatch  = useDispatch();
 
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }
+      else{
+
+        dispatch(logout(userData))
+      }
+
+    })
+    .finally(()=>setLoading(false))
+
+  },[])
+
+
+  return !loading ?<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        TODO:{/* <Outlet/> */}
+      </main>
+      <Footer/>
+
+    </div>
+  </div>:null
 }
 
 export default App
